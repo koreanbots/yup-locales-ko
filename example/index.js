3a8a611217632b0938374b5b605cc6b519fa5ffe
원더 <wonderbotsupport@naver.com>
2021-01-24 22:19:27 +0900

    fix: module to commonjs

--- example/index.js
+++ example/index.js
@@ -4,13 +4,16 @@ import yupLocalesKo from '../index.js'
 yup.setLocale(yupLocalesKo)
 
 const schema = yup.object().shape({
-  name: yup.string(),
+  name: yup.string().required(),
   age: yup.number().min(18),
 })
 
-schema.validateSync({
-  name: 'Wonderlandpark',
-  age: 1
+const res = schema.validateSync({
+  name: 'Hi',
+  age: '20'
+}, {
+  abortEarly: false
 })
 
+console.log(res)
 // ValidationError: age은(는) 18보다 크거나 같아야합니다
--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-import printValue from './printValue.js';
+const printValue = require('./printValue.js')
 
 
-export let mixed = {
+let mixed = {
   default: '${path}(이)가 올바르지 않습니다',
   required: '${path}은(는) 필수 항목입니다',
   oneOf: '${path}은(는) 다음 값 중 하나여야합니다: ${values}',
   notOneOf: '${path}은(는) 다음 값이 아니여야합니다: ${values}',
   notType: ({ path, type, value, originalValue }) => {
-    let isCast = originalValue != null && originalValue !== value;
+    let isCast = originalValue != null && originalValue !== value
     let msg =
       `${path}는 \`${type}\` 타입이여야합니다., ` +
       `하지만 마지막 값은 \`${printValue(value, true)}\`이었습니다.` +
       (isCast
         ? ` (\`${printValue(originalValue, true)}\` 값에서 변환됨).`
-        : '.');
+        : '.')
 
     if (value === null) {
-      msg += `\n "null"이 빈 값으로 의도된 경우 스키마를 \`.nullable()\`로 표시해야합니다.`;
+      msg += `\n "null"이 빈 값으로 의도된 경우 스키마를 \`.nullable()\`로 표시해야합니다.`
     }
 
-    return msg;
+    return msg
   },
   defined: '${path}을(를) 정의해야 합니다.',
-};
+}
 
-export let string = {
+exports.mixed = mixed
+
+let string = {
   length: '${path}은(는) ${length}자여야합니다',
   min: '${path}은(는) 최소 ${min}자여야합니다',
   max: '${path}은(는) 최대 ${max}자여야합니다',
@@ -35,9 +37,11 @@ export let string = {
   trim: '${path}은(는) 띄어쓰기를 시작과 끝에 포함하지 않는 문자열이어야합니다',
   lowercase: '${path}은(는) 소문자 문자열이어야합니다',
   uppercase: '${path}은(는) 대문자 문자열이어야합니다',
-};
+}
 
-export let number = {
+exports.string = string
+
+let number = {
   min: '${path}은(는) ${min}보다 크거나 같아야합니다',
   max: '${path}은(는) ${max}보다 작거나 같아야합니다',
   lessThan: '${path}은(는) ${less}보다 작아야합니다',
@@ -45,28 +49,38 @@ export let number = {
   positive: '${path}은(는) 양수여야합니다',
   negative: '${path}은(는) 음수여야합니다',
   integer: '${path}은(는) 정수여야합니다',
-};
+}
 
-export let date = {
+exports.number = number
+
+let date = {
   min: '${path} 필드는 ${min}보다 미래여야합니다',
   max: '${path} 필드는 ${max}보다 과거여야합니다',
-};
+}
 
-export let boolean = {
+exports.date = date
+
+let boolean = {
   isValue: '${path} 필드는 ${value}(이)여야합니다',
-};
+}
 
-export let object = {
+exports.boolean = boolean
+
+let object = {
   noUnknown: '${path} 필드에 지정되지 않은 키가 있습니다: ${path}',
-};
+}
 
-export let array = {
+exports.object = object
+
+let array = {
   min: '${path} 필드에는 ${min}개 이상의 항목이 있어야 합니다.',
   max: '${path} 필드에는 ${max}개보다 작거나 같아야 합니다.',
   length: '${path}에는 ${length}개의 항목이 있어야 합니다.',
-};
+}
 
-export default Object.assign(Object.create(null), {
+exports.array = array
+
+module.exports = {
   mixed,
   string,
   number,
@@ -74,4 +88,4 @@ export default Object.assign(Object.create(null), {
   object,
   array,
   boolean,
-});
+}
